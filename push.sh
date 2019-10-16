7cbeac5beea12dc7de84f2aa30afc34990241cbd
Philippe Marcotte <phmarm@pop-os.localdomain>
2019-10-16 16:15:33 -0400

    push script

---
+++ push.sh
@@ -0,0 +1,8 @@
+git add -u
+git commit -m "$1"
+git push
+
+dts devel build --push -u philmarc
+docker -H daffybot pull philmarc/philippe-ros-core -a
+dts duckiebot demo --demo_name pure_pursuit --package_name pure_pursuit --duckiebot_name daffybot --image philmarc/philippe-ros-core:v1-arm32v7
+docker -H daffybot logs -f demo_pure_pursuit

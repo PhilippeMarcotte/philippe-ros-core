446b64b5d9d6bbae626a664f387bb08fce174b69
Philippe Marcotte <phmarm@pop-os.localdomain>
2019-10-16 17:59:10 -0400

    tweaking offsets

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 git push
 
 dts devel build --push -u philmarc
-docker -H daffybot pull philmarc/philippe-ros-core -a
+docker -H daffybot.local pull philmarc/philippe-ros-core -a
 dts duckiebot demo --demo_name pure_pursuit --package_name pure_pursuit --duckiebot_name daffybot --image philmarc/philippe-ros-core:v1-arm32v7
-docker -H daffybot logs -f demo_pure_pursuit
+docker -H daffybot.local logs -f demo_pure_pursuit
